e2ddc606c0550a5e6a3bbe3e82de0e9d34d3020c
GG23800 <jeromechopen@hotmail.com>
2022-05-01 23:10:37 +0200

    add full constructor methods

--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -20,13 +20,17 @@ class Matrix
     public:
         T **data;
 
-        Matrix(unsigned int nol=1, unsigned int noc=1, T value=T(0);
-        ~Matrix();
+        Matrix(unsigned int nol=1, unsigned int noc=1, T value=T(0); // constructor
+        Matrix(const Matrix<T> &input); // copy constructor
+        Matrix(Matrix<T> &&input); // move constructor
+        Matrix& operator= (const Matrix<T> &input); // copy assignement
+        Matrix& operator= (Matrix<T> &&input); // move assignement
+        ~Matrix(); // destructor
 
         inline unsigned int sizel() const {return NumberOfLine;}
         inline unsigned int sizec() const {return NumberOfColumn;}
         void resize(unsigned int nol, unsigned int noc);
-        void reset(T nv);
+        void reset(T nv=T(0));
     private:
         unsigned int NumberOfLine;
         unsigned int NumberOfColumn;
@@ -37,16 +41,113 @@ Matrix<T>::Matrix(unsigned int nol, unsigned int noc, T value)
 {
     NumberOfLine = nol;
     NumberOfColumn = noc;
-    if ((nol == 0) || (noc == 0)) {data = nullptr;}
+    if ((nol == 0) || (noc == 0)) 
+    {
+        NumberOfLine = 0;
+        NumberOfColumn = 0;
+        data = nullptr;
+    }
     else
     {
         data = new T*[NumberOfLine]();
-        for (unsigned int k=0 ; k<NumberOfLine ; k++) {data[k] = new T[NumberOfColumn];}
+        for (unsigned int k=0 ; k<NumberOfLine ; k++) {data[k] = new T[NumberOfColumn]();}
         for (unsigned int k=0 ; k<NumberOfLine ; k++) 
         {
             for (unsigned int l=0 ; l<NumberOfColumn ; l++) {data[k][l] = value;}
         }
     }
+}
+
+template <class T>
+Matrix<T>::Matrix(const Matrix<T> &input)
+{
+    if ( (input.NumberOfLine == 0) || (input.NumberOfColumn == 0) )
+    {
+        NumberOfLine = 0;
+        NumberOfColumn = 0;
+        data = nullptr;
+    }
+    else if (input.NumberOfLine == NumberOfLine)
+    {
+        if (input.NumberOfColumn != NumberOfColumn)
+        {
+            NumberOfColum = input.NumberOfColumn;
+            for (unsigned int k=0 ; l<NumberOfLine ; k++)
+            {
+                delete[] data[k];
+                data[k] = new T[NumberOfColumn]();
+            }
+        }
+    }
+    else
+    {
+        for (unsigned int k=0 ; k<NumberOfLine ; k++) {delete[] data[k];}
+        NumberOfLine = input.NumberOfLine;
+        NumberOfColumn = input.NumberOfColumn;
+        delete[] data;
+        data = new T*[NumberOfLine]();
+        for (unsigned int k=0 ; k<NumberOfLine ; k++) {data[k] = new T[NumberOfColumn]();}
+    }
+    for (unsigned int k=0 ; k<NumberOfLine ; k++) 
+    {
+        for (unsigned int l=0 ; l<NumberOfColumn ; l++) {data[k][l] = input.data[k][l];}
+    }
+}
+
+template <class T>
+Matrix<T>::Matrix(Matrix<T> &&input)
+{
+    NumberOfLine = input.NumberOfLine;
+    NumberOfColumn = input.NumberOfColumn;
+    data = std::move(input.data);
+    input.data = nullptr;
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator= (const Matrix<T> &input)
+{
+    if ( (input.NumberOfLine == 0) || (input.NumberOfColumn == 0) )
+    {
+        NumberOfLine = 0;
+        NumberOfColumn = 0;
+        data = nullptr;
+    }
+    else if (input.NumberOfLine == NumberOfLine)
+    {
+        if (input.NumberOfColumn != NumberOfColumn)
+        {
+            NumberOfColum = input.NumberOfColumn;
+            for (unsigned int k=0 ; l<NumberOfLine ; k++)
+            {
+                delete[] data[k];
+                data[k] = new T[NumberOfColumn]();
+            }
+        }
+    }
+    else
+    {
+        for (unsigned int k=0 ; k<NumberOfLine ; k++) {delete[] data[k];}
+        NumberOfLine = input.NumberOfLine;
+        NumberOfColumn = input.NumberOfColumn;
+        delete[] data;
+        data = new T*[NumberOfLine]();
+        for (unsigned int k=0 ; k<NumberOfLine ; k++) {data[k] = new T[NumberOfColumn]();}
+    }
+    for (unsigned int k=0 ; k<NumberOfLine ; k++) 
+    {
+        for (unsigned int l=0 ; l<NumberOfColumn ; l++) {data[k][l] = input.data[k][l];}
+    }
+    return *this;
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator= (Matrix<T> &&input)
+{
+    NumberOfLine = input.NumberOfLine;
+    NumberOfColumn = input.NumberOfColumn;
+    data = std::move(input.data);
+    input.data = nullptr;   
+    return *this;
 }
 
 template <class T>
--- src/Vector.hpp
+++ src/Vector.hpp
@@ -20,12 +20,16 @@ class Vector
     public:
         T *data;
 
-        Vector(unsigned int noe=1, T value=T(0);
-        ~Vector();
+        Vector(unsigned int noe=1, T value=T(0); // constructor
+        Vector(const Vector<T> &input); // copy constructor
+        Vector(Vector<T> &&input); // move constructor
+        Vector& operator= (const Vector<T> &input); // copy affectation
+        Vector& operator= (Vector<T> &&input); // move affectation
+        ~Vector(); // destructor
 
         inline unsigned int size() const {return NumberOfElement;}
         void resize(unsigned int noe);
-        void reset(T nv);
+        void reset(T nv=T(0));
     private:
         unsigned int NumberOfElement;
 }
@@ -40,6 +44,60 @@ Vector<T>::Vector(unsigned int noe, T value)
         data = new T[NumberOfElemet]();
         for (unsigned int k=0 ; k<NumberOfElement ; k++) {data[k] = value;}
     }
+}
+
+template <class T>
+Vector<T>::Vector(const Vector<T> &input)
+{
+    if (input.NumberOfElement == 0)
+    {
+        NumberOfElement = 0;
+        delete[] data;
+        data = nullptr;
+    }
+    else if (input.NumberOfElement != NumberOfElement)
+    {
+        NumberOfElement = input.NumberOfElement;
+        delete[] data;
+        data = new T[NumberOfElement]();
+    }
+    for (unsigned int k=0 ; k<NumberOfElement ; k++) {data[k] = input.data[k];}
+}
+
+template <class T>
+Vector<T>::Vector(Vector<T> &&input)
+{
+    NumberOfElement = input.NumberOfElement;
+    data = std::move(input.data);
+    input.data = nullptr;
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator= (const Vector<T> &input)
+{
+    if (input.NumberOfElement == 0)
+    {
+        NumberOfElement = 0;
+        delete[] data;
+        data = nullptr;
+    }
+    else if (input.NumberOfElement != NumberOfElement)
+    {
+        NumberOfElement = input.NumberOfElement;
+        delete[] data;
+        data = new T[NumberOfElement];
+    }
+    for (unsigned int k=0 ; k<NumberOfElement ; k++) {data[k] = input.data[k];}
+    return *this;
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator= (Vector<T> &&input)
+{
+    NumberOfElement = input.NumberOfElement;
+    data = std::move(input.data);
+    input.data = nullptr;
+    return *this;
 }
 
 template <class T>
@@ -59,12 +117,8 @@ Vector<T>::resize(unsigned int noe)
     if (NumberOfElement != noe)
     {
         NumberOfElement = noe;
-        if (data == nullptr) {data = new T[N]();}
-        else
-        {
-            delete[] data;
-            data = new T[NumberOfElement]();
-        }
+        if (data != nullptr) {delete[] data;}
+        data = new T[NumberOfElement]();
     }
 }
 
